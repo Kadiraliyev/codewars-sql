157c403685092865efee4302e5a196546b779563
Arman <arman.k1294@gmail.com>
2024-08-02 14:42:59 +0500

    chore: Add SQL query to count vowels in a string

---
+++ 7kyu.sql
@@ -0,0 +1,23 @@
+-- ❓ Return the number (count) of vowels in the given string.
+-- We will consider a, e, i, o, u as vowels for this Kata (but not y).
+-- The input string will only consist of lower case letters and/or spaces.
+-- ✅ Solution:
+SELECT
+  str,
+  LENGTH (str) - LENGTH (
+    REPLACE (
+      REPLACE (
+        REPLACE (
+          REPLACE (REPLACE (str, 'a', ''), 'e', ''),
+          'i',
+          ''
+        ),
+        'o',
+        ''
+      ),
+      'u',
+      ''
+    )
+  ) AS res
+FROM
+  getcount;
