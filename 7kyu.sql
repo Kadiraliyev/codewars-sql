1d5b2456c70bf85b762e9f1b4b7c0832a8ecb0f5
Arman <arman.k1294@gmail.com>
2024-08-02 14:48:46 +0500

    chore: Update SQL queries to include necessary joins and sub-queries

--- 7kyu.sql
+++ 7kyu.sql
@@ -21,3 +21,85 @@ SELECT
   ) AS res
 FROM
   getcount;
+
+-- ❓ For this challenge you need to create a simple SELECT statement that will return all columns from the products table, and join to the companies table so that you can return the company name.
+-- products table schema
+-- id
+-- name
+-- isbn
+-- company_id
+-- price
+-- companies table schema
+-- id
+-- name
+-- You should return all product fields as well as the company name as "company_name".
+-- NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+-- ✅ Solution:
+SELECT
+  products.*,
+  companies.name AS company_name
+FROM
+  products
+  JOIN companies ON products.company_id = companies.id;
+
+-- ❓ For this challenge you need to create a SELECT statement, this SELECT statement will use an IN to check whether a department has had a sale with a price over 90.00 dollars BUT the sql MUST use the WITH statement which will be used to select all columns from sales where the price is greater than 90.00, you must call this sub-query special_sales.
+-- departments table schema
+-- id
+-- name
+-- sales table schema
+-- id
+-- department_id (department foreign key)
+-- name
+-- price
+-- card_name
+-- card_number
+-- transaction_date
+-- resultant table schema
+-- id
+-- name
+-- NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+-- ✅ Solution:
+WITH
+  special_sales AS (
+    SELECT
+      *
+    FROM
+      sales
+    WHERE
+      price > 90
+  )
+SELECT
+  *
+FROM
+  departments
+WHERE
+  id IN (
+    SELECT
+      department_id
+    FROM
+      special_sales
+  );
+
+--  ❓ You have access to a table of monsters as follows:
+-- monsters schema
+-- id
+-- name
+-- legs
+-- arms
+-- characteristics
+-- The monsters in the provided table have too many characteristics, they really only need one each. Your job is to trim the characteristics down so that each monster only has one. If there is only one already, provide that. If there are multiple, provide only the first one (don't leave any commas in there).
+-- You must return a table with the format as follows:
+-- output schema
+-- id
+-- name
+-- characteristic
+-- Order by id
+-- ✅ Solution:
+SELECT
+  id,
+  name,
+  split_part (characteristics, ',', 1) as characteristic
+FROM
+  monsters
+ORDER BY
+  id
