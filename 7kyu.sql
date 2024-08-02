8bfb7bc86ce1d936eb25f111007957263715626f
Arman <arman.k1294@gmail.com>
2024-08-02 19:17:03 +0500

    chore: Update demographics table to convert text fields to bit length

--- 7kyu.sql
+++ 7kyu.sql
@@ -103,3 +103,26 @@ FROM
   monsters
 ORDER BY
   id
+
+
+-- ❓ Given a demographics table in the following format:
+
+-- ** demographics table schema **
+
+-- id
+-- name
+-- birthday
+-- race
+-- you need to return the same table where all text fields (name & race) are changed to the bit length of the string.
+
+-- ✅ Solution:
+
+SELECT 
+    id,
+    -- Convert the name field to its bit length
+    LENGTH(name) * 8 AS name,
+    -- Convert the race field to its bit length
+    LENGTH(race) * 8 AS race,
+    birthday
+FROM 
+    demographics;
